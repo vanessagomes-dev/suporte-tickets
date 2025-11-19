0f1a91bfbce682772db71a212828b3f469974b21
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-11-19 20:07:43 -0300

    refactor(router): Refina extração de parâmetros de rota
    Aprimora o tratamento de parâmetros dinâmicos da URL para uso no route handler.
    
    * Cria a função **extractcarrie.params.js** (ou similar) na pasta `utils` para encapsular a lógica de extração de valores dos parâmetros.
    * **Importa** a função e a utiliza no **route handler** para processar a URL da requisição.
    * Garante que os parâmetros extraídos sejam verificados e, se presentes, sejam anexados à requisição (ex: `req.params`) para fácil acesso pelo controller.
    * Isso facilita a implementação de rotas com filtros e busca por ID (GET /tickets/:id).

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -1,4 +1,7 @@
 export function index({ request, response, database }) {
+    const { status } = request.query
+    
+
     const tickets = database.select("tickets")
 
     return response.end(JSON.stringify(tickets))
